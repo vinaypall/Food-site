6313a229d49abbafcf5d62f24804aa156b5d43ec
Vinay pal <88939141+vinaypall@users.noreply.github.com>
2023-03-20 21:27:53 +0530

    Add files via upload

---
+++ recipe.js
@@ -0,0 +1,39 @@
+export const my_recipe = {
+  recipe_1: {
+    name: 'Chirashi Don',
+    preparation_time: '30 minutes',
+    servings: '2',
+    ingredients: [
+      '1/2 cup rice',
+      '1/2 cup water',
+      '2 eggs',
+      '300g of salmon, tuna and salmon roe',
+    ],
+    instructions: [
+      'Cook rice and water in a rice cooker',
+      'Fry eggs in a pan',
+      'Serve rice, eggs and fish on a plate',
+    ],
+ 
+  },
+   recipe_2: {
+    name: 'Gulabi Chai',
+    preparation_time: '15 minutes',
+    servings: '1',
+    ingredients: [
+      '1/2 cup milk',
+      '1/2 cup water',
+      '2 Spoons Sugar',
+      '1 Spoon Tea dust',
+      '5 dry leaves of Rose',
+      'Tea masala',
+    ],
+    instructions: [
+      'boil Water',
+      'Add sugar ,Tea Masala,Tea dust and dried Roses',
+      'Add milk boil for 5 minutes',
+      'Ready to serve',
+    ],
+ 
+  },
+}
